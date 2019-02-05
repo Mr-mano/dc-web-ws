597592105e477057c343af6a75d2adabd4db5c0d
Mr-mano <mano.quere@orange.fr>
2019-02-05 15:37:32 +0000

    projet mail sourds et muets

--- server.py
+++ server.py
@@ -3,9 +3,53 @@ from flask_socketio import SocketIO, send, emit
 from flask import render_template
 import time
 import threading
+from imapclient import IMAPClient
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+DEBUG = True
+ 
+HOSTNAME = 'imap.gmail.com'
+USERNAME = ''
+PASSWORD = ''
+MAILBOX = 'Inbox'
+ 
+NEWMAIL_OFFSET = 0   # my unread messages never goes to zero, yours might
+MAIL_CHECK_FREQ = 10 # check mail every 60 seconds
+ 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GREEN_LED = 18
+RED_LED = 24
+GPIO.setup(GREEN_LED, GPIO.OUT)
+GPIO.setup(RED_LED, GPIO.OUT)
+ 
+def loop():
+    server = IMAPClient(HOSTNAME, use_uid=True, ssl=True)
+    server.login(USERNAME, PASSWORD)
+ 
+    if DEBUG:
+        socketio.emit('alert', 'Logging in as ' + USERNAME, Broadcast=True)
+        select_info = server.select_folder(MAILBOX)
+        socketio.emit('alert','%d messages in INBOX' % select_info[b'EXISTS'], Broadcast=True)
+ 
+    folder_status = server.folder_status(MAILBOX, 'UNSEEN')
+    newmails = int(folder_status[b'UNSEEN'])
+ 
+    if DEBUG:
+        socketio.emit('alert','You have'+ str(newmails)+ 'new emails!', Broadcast=True)
+ 
+    if newmails > NEWMAIL_OFFSET:
+        GPIO.output(GREEN_LED, True)
+        GPIO.output(RED_LED, False)
+    else:
+        GPIO.output(GREEN_LED, False)
+        GPIO.output(RED_LED, True)
+ 
+    time.sleep(MAIL_CHECK_FREQ)
+
 
 @app.route("/")
 def index():
@@ -14,12 +58,12 @@ def index():
 
 def message_loop():
     while True:
-        message = input('Votre message ?')
-        socketio.emit('alert', message, Broadcast=True)
+        loop()
 
 # Vue que notre méthode pour lire nos message est une boucle infinie
 # Elle bloquerait notre serveur. Qui ne pourrait répondre à aucune requête.
 # Ici nous créons un Thread qui va permettre à notre fonction de se lancer 
 # en parallèle du serveur.
-read_messages = threading.Thread(target=message_loop)
-read_messages.start()
+
+read_loop = threading.Thread(target=message_loop)
+read_loop.start()
--- static/app.js
+++ static/app.js
@@ -1,16 +1,21 @@
 $(function () {
 	socket = io.connect('http://' + document.domain + ':' + location.port);
 	socket.on('connect', function() {
-		$('#status').text('Connecté');
+		$('#status').html('Connecté');
     	socket.emit('client_connected', {data: 'New client!'});
 	});
 
 	socket.on('disconnect', function() {
-		$('#status').text('Déconnecté');
+		$('#status').html('Déconnecté');
 	});
 
 	socket.on('alert', function (data) {
-    	$('#status').text('Connecté');
-        $('#content').append(data + "<br />");
+    	$('#status').html('Connecté');
+        $('#content').html(data + "<br />");
 	});
+
+        socket.on('loop', function (data) {
+        $('#content').html(data + "<br />");
+        });
+
 });
